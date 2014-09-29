3e431169bf8369c8b49b93301be3d4deed2d3a9e
mlauter <lauter.miriam@gmail.com>
2014-09-29 12:19:39 -0400

    implement colloquy style join messages

--- client.py
+++ client.py
@@ -2,6 +2,29 @@ import socket
 import asyncio
 import sys
 
+def translate(msg):
+    cmd, *msg = msg.strip().split()
+
+    if cmd == '/join':
+        # not going to allow multiple joins for now
+        channel = msg[0]
+        if channel[0] != '#':
+            raise Exception('channels must begin with #')
+        if len(msg) == 2:
+            password = msg[1]
+            return 'JOIN ' + channel + ' ' + password + '\r\n'
+        else:
+            return 'JOIN ' + channel + '\r\n' 
+
+    elif cmd == '/msg':
+        pass
+    elif cmd == '/leave':
+        pass
+    elif cmd == '/whois':
+        pass
+    else:
+        raise Exception("unknown command")
+
 class IRCProtocol(asyncio.Protocol):
 
     def __init__(self,*args,**kwargs):
@@ -17,8 +40,12 @@ class IRCProtocol(asyncio.Protocol):
         loop.add_reader(sys.stdin, self.user_input)
 
     def user_input(self):
-        user_msg = sys.stdin.readline() + '\r\n'
-        self.trans.write(user_msg.encode())
+        user_msg = sys.stdin.readline()
+        try:
+            msg_to_send = translate(user_msg)
+            self.trans.write(msg_to_send.encode())
+        except Exception as e:
+            print(e)
 
     def data_received(self, data):
         '''Add any new messages to the buffer. Split on new lines, pop all messages ending in newline off the buffer, return the remainder (incomplete message) to the buffer. If PING is found, respond PONG.'''
@@ -39,53 +66,14 @@ class IRCProtocol(asyncio.Protocol):
     def connection_lost(self, exc):
         print("Connection lost!!")
 
-
-# class IRCClient(object):
-
-#     def __init__(self):
-#         self.get_connect_info()
-#         self.port = 6667
-#         self.socket = socket.socket()
-
-#         self.socket.connect((self.host, self.port))
-#         #password?
-#         self.socket.send(str.encode('NICK %s \r\n' % self.nickname))
-#         self.socket.send(str.encode('USER %(nick)s 8 * :%(real)s\r\n'% {'nick':self.nickname,'real':self.realname}))
-
-
-#     def pingPong(self):
-        
-#             msg = self.socket.recv(4096).decode()
-#             lines = msg.split('\n')
-
-#             for data in lines:
-#                 data = data.strip()
-
-#                 if data == '':
-#                     continue
-#                 print(data)
-
-#                 if data.find('PING') != -1:
-#                     self.socket.send(str.encode('PONG %s' % data[5:]))
-
-#     def get_connect_info(self):
-#         self.host = input("Enter the server to connect to\n")
-#         connected = False
-#         self.nickname = input("Enter your nickname\n")
-#         self.realname = input("Enter your real name\n")
-
-
-
 loop = asyncio.get_event_loop()
 coro = loop.create_connection(IRCProtocol, host="irc.freenode.net", port=6667)
 loop.run_until_complete(coro)
-# loop.create_connection(IRCProtocol, host="localhost", port=1234)
 
 try:
     loop.run_forever()
 finally:
     loop.close()
 
-# IRCClient()
 
 
